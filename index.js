a9ced6548aee6a51c1306248c53607d13dd9d8cd
E. A. Arroyo <hi.ed@hotmail.com>
2026-03-11 21:23:12 -0400

    change iso value

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const MARGIN = 50;
 const GRID_MIN = -1;
 const GRID_MAX = 1;
 const STEP = 0.1;
-const ISO_VALUE = 1;
+const ISO_VALUE = 0.8;
 const DRAW_CONTOUR = true;
 const DRAW_DISTANCE = true;
 
